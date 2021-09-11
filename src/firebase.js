26ea51ac463b624386bb4d526615db013770e889
hyeonseung-lee <dlgustmd3590@kookmin.ac.kr>
2021-09-12 00:15:30 +0900

    finish #1

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/compat/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGIN_ID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
